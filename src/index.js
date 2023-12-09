00465e587d7165d5f359f04fc6dcffecf4f8b612
Derrick <mereo83@gmail.com>
2023-12-09 12:39:00 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './root';
-import Pricing from './components/pricing';
+import {Pricing} from './components/pricing';
 import reportWebVitals from './reportWebVitals';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
@@ -11,11 +11,19 @@ import Compareplans from './components/compareplan';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <body>
     <Navbar></Navbar>
     <Root></Root>
-    <Pricing> </Pricing> 
+    <main>
+    <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
+    <Pricing planName="Free" price="0" users="10" storage="2" support="Email support" helpCenter="Help center access" buttonText="Sign up for free"> </Pricing>
+    <Pricing planName="Pro" price="15" users="20" storage="10" support="Priority email support" helpCenter="Help center access" buttonText="Get started"> </Pricing>
+    <Pricing planName="Enterprise" price="29" users="30" storage="15" support="Phone and email support" helpCenter="Help center access" buttonText="Contact us" highlight> </Pricing> 
+    </div>
     <Compareplans></Compareplans>
+    </main>
     <Footer></Footer>
+    </body>
   </React.StrictMode>
 );
 
